4f165aeec7d114205999d56c846a41d91ad51dec
Randall Reed, Jr <randallreedjr@gmail.com>
2016-12-29 14:29:09 -0500

    Add HelloWorld component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld.js';
 
 import HelloWorldContainer from './HelloWorldContainer.js';
 
